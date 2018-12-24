1fad6591745e326d439f7a526c37042bb360b663
lxwNO1 <lxwNO1@github.com>
2018-12-24 19:12:12 +0800

    testNO.1 :poject

---
+++ springboot-dubbon-mybatis-provider/src/main/java/com/lxw/SpringbootDubbonMybatisProviderApplication.java
@@ -0,0 +1,14 @@
+package com.lxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDubbonMybatisProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDubbonMybatisProviderApplication.class, args);
+    }
+
+}
+
